f97d71569a809725ee99a654a0aded15d8464ddc
LDHHHHH <ars6789@gmail.com>
2020-08-11 10:17:41 +0900

    kamtec api key 변경

--- src/main/java/com/message/kamtec/mapper/KamtecKakaoRepository.java
+++ src/main/java/com/message/kamtec/mapper/KamtecKakaoRepository.java
@@ -12,7 +12,7 @@ public interface KamtecKakaoRepository extends JpaRepository<KakaoMessageModel, 
 
 	@Query(value="insert into ita_talk_tran " + 
 				"(date_client_req, content, msg_status, recipient_num, msg_type, sender_key, template_code, country_code)" + 
-				"values(getdate(), :content, '1', :recipient_num,  '1008',  'c0eaebb438dbc16a5a99e33ff827f1b20c4aeb31', :template_code, '82')"	, 
+				"values(getdate(), :content, '1', :recipient_num,  '1008',  '4d2238c23c1663f71f6f5d1a671aa315d5b457c7', :template_code, '82')"	, 
 				nativeQuery = true)
 	public void sendMessage(@Param("template_code") String template_code,
 							 @Param("recipient_num") String recipient_num,
