96591e7e3581e3b9915ab0e960e0241490b641a7
Min Mike <minssan9@gmail.com>
2020-09-01 11:14:17 +0900

    message List 로 받기

--- src/main/java/com/message/controller/MessageRestController.java
+++ src/main/java/com/message/controller/MessageRestController.java
@@ -2,17 +2,13 @@ package com.message.controller;
 
 import com.message.dto.MessageDto;
 import com.message.service.MessageService;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 @RequestMapping("/message")
 @Slf4j 
@@ -22,8 +18,10 @@ class MessageRestController {
 	private MessageService messageService; 
 
 	@PostMapping("/save")
-	public @ResponseBody ResponseEntity<String> sendMessage(@RequestBody MessageDto messageDto ) throws Exception {
-		messageService.save(messageDto);
-		return new ResponseEntity<String>(messageDto.getResult(), HttpStatus.OK);
+	public @ResponseBody ResponseEntity<List<MessageDto>> sendMessage(@RequestBody List<MessageDto> messageDtos ) throws Exception {
+		for (MessageDto messageDto:messageDtos) {
+			messageService.save(messageDto);
+		}
+		return new ResponseEntity<List<MessageDto>>(messageDtos, HttpStatus.OK);
 	} 
 }
